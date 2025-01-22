3199161c3649e5f5deeb16e096b7cfd0572c7898
EdwinBayingana <bayinganaedwin@gmail.com>
2025-01-23 00:38:48 +0200

    file and project refactoring 2

--- models/state.py
+++ models/state.py
@@ -1,18 +1,35 @@
+#!/usr/bin/python3
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 import models
-from os import getenv
+from models.city import City
+import shlex
+
 
 class State(BaseModel, Base):
-    """ Represents a state """
-    __tablename__ = 'states'
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """ Getter for cities related to the state """
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
