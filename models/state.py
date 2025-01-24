8e35210fce561ab28daaa20cfdd3266976987507
Avellin003 <uavellinbonaparte@gmail.com>
2025-01-24 08:12:45 +0200

    tables

--- models/state.py
+++ models/state.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from models.city import City
-from os import getenv
 
-storage = getenv("HBNB_TYPE_STORAGE")
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    if storage == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref='states')
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+
     else:
-        name = ""
-        cities = []
-    if storage != 'db':
         @property
         def cities(self):
-            """
-            return list of cities
-            """
-            from models import storage
-            from models.city import City
-            list_cities = []
-            cities = storage.all(City)
-            for key, city_obj in cities.items():
-                if city_obj.state_id == self.id:
-                    list_cities.append(city_obj)
-            return list_cities
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
