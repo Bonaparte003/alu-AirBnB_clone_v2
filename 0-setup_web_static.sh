a9554a03d98ecf38c2be084c7c047541eb740949
Avellin003 <uavellinbonaparte@gmail.com>
2025-02-08 12:16:09 +0200

    debuggin

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# sets up servers for deployment
+# sets up servers for deplyoment
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
@@ -14,14 +14,29 @@ sudo echo "<html>
   <head>
   </head>
   <body>
-    Holberton School
+    Bonaparte UTERAMAHORO
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo echo '
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/Forecast;
+	location /hbnb-static {
+		alias /data/web_static/current/;
+
+}
+        index index.html index.htm 404.html index.nginx-debian.html;
+        server_name _;
+        add_header X-Served-By $HOSTNAME;
+        error_page 404 /5-design_a_beautiful_404_page.html;
+}
+
+' > /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
