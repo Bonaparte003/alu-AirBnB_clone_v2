8c4730f50bb0a9ee345117dfe5e55bdb84189541
Avellin003 <uavellinbonaparte@gmail.com>
2025-02-21 12:32:12 +0200

    updates

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 from flask import Flask
+"""Start a Flask web application"""
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Print Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Print HBNB!"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """C text"""
     return "C " + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
+    """Python is cool"""
     return "Python " + text.replace('_', ' ')
 
 
