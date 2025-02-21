c9386db2ca21bdc0811c5fa06a3b538213d196aa
Bonaparte <b.uteramaho@alustudent.com>
2025-02-21 12:47:40 +0200

    updates

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
+"""Starts the web app"""
 from flask import Flask
-"""Start a Flask web application"""
+from urllib.parse import unquote
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Print Hello HBNB!"""
-    return "Hello HBNB!"
+    """Displays the Hello HBNB"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Print HBNB!"""
-    return "HBNB"
+def display_hbnb():
+    """Returns the HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """C text"""
-    return "C " + text.replace('_', ' ')
+def display_c(text):
+    """Replace underscores with spaces"""
+    text = unquote(text).replace('_', ' ')
+    return 'C {}'.format(text)
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """Python is cool"""
-    return "Python " + text.replace('_', ' ')
+def display_python(text):
+    """Replace underscores with spaces"""
+    text = unquote(text).replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
